5f683b23250d6d14dc508978fb7c5dd2eb831770
Gravity <isaakhern@outlook.com>
2025-11-25 01:12:54 -0500

    Added hands.py hand bounding box + tracking

--- hand.py
+++ hand.py
@@ -27,19 +27,28 @@ else:
         success, frame = cap.read()  # Read a frame
 
         if success:
-            RGB_frame = cv.cvtColor(frame, cv.COLOR_BGR2RGB)
+            RGB_frame = cv.cvtColor(frame, cv.COLOR_BGR2RGB)  # H, W, C
             result = hand.process(RGB_frame)
 
             if result.multi_hand_landmarks:
                 for hand_landmarks in result.multi_hand_landmarks:
-                    print(hand_landmarks)
+                    x_min = float('inf')
+                    y_min = float('inf')
+                    x_max = float('-inf')
+                    y_max = float('-inf')
+                    for lm in hand_landmarks.landmark:
+                        x_min = int(min(x_min, lm.x * frame.shape[1]))
+                        y_min = int(min(y_min, lm.y * frame.shape[0]))
+                        x_max = int(max(x_max, lm.x * frame.shape[1]))
+                        y_max = int(max(y_max, lm.y * frame.shape[0]))
                     mp_drawing.draw_landmarks(
-                        frame, hand_landmarks, mp_hands)
-
+                        frame, hand_landmarks, mp_hands.HAND_CONNECTIONS)
+                    cv.rectangle(frame, (x_min, y_min),
+                                 (x_max, y_max), (0, 0, 255), 3)
             cv.imshow('Live Video Feed', frame)  # Display the frame
 
             # Press 'q' to exit
-            if cv.waitKey(25) & 0xFF == ord('q'):
+            if cv.waitKey(100) & 0xFF == ord('q'):
                 break
         else:
             break
